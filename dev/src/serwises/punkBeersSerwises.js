95bc7c197c733f56e46d8ef78498486cd1afa362
Kniferrr <Dragon.ry0204@yandex.ru>
2022-07-22 18:07:13 +0300

    reduced the number of elements to 24 per page

--- dev/src/serwises/punkBeersSerwises.js
+++ dev/src/serwises/punkBeersSerwises.js
@@ -1,9 +1,9 @@
 export default class punkBeersSerwises {
     _apiBase = 'https://api.punkapi.com/v2';
-    
+    perPage = "&per_page=24";
   
     async getAllBeers(url) {
-      const res = await fetch(`${this._apiBase}${url}`);
+      const res = await fetch(`${this._apiBase}${url}${this.perPage}`);
   
       if (!res.ok) {
         throw new Error(`Could not fetch ${url}` +
